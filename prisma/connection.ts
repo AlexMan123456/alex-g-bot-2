331d94be59773b1877240c36bfb1063430532297
AlexMan123456 <alex.man03888@gmail.com>
2025-12-06 13:35:50 +0000

    Migrate to Prisma v7
    This is not quite ready yet. I think I've done most of the configuration by now, but the database commands themselves aren't working for whatever reason and I'm not entirely sure why.

--- prisma/connection.ts
+++ prisma/connection.ts
@@ -1,4 +1,9 @@
-import { PrismaClient } from "generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "generated/prisma/client";
 
-const database = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const database = new PrismaClient({ adapter });
+
 export default database;
